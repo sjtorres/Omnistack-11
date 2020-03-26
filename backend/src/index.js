23d2770d3efd0e5f88fa864bba5254ca129a92fa
Sandro Torres <sandrotorres.ti@gmail.com>
2020-03-26 10:11:12 -0300

    BackEnd finalizado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 // Importando módulos
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 
 // Criando a aplicação
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
