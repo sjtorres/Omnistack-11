c88bee724ce17df32e28cc51fc5a01b560612497
Sandro Torres <sandrotorres.ti@gmail.com>
2020-03-29 19:31:18 -0300

    Atualizado as Pages Incidents

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png'
 
@@ -11,9 +11,33 @@ export default function Incidents() {
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total de 0 casos.
+          Total de <Text style={styles.headerTextBold}>0 casos.</Text>
         </Text>
       </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+
+      <View style={styles.incidentList}>
+        <View style={styles.incident}>
+          <Text style={styles.incidentProperty}>ONG:</Text>
+          <Text style={styles.incidentValue}>APAD</Text>
+
+          <Text style={styles.incidentProperty}>CASO:</Text>
+          <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+          <Text style={styles.incidentProperty}>VALOR:</Text>
+          <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+          <TouchableOpacity 
+            style={styles.detailsButton} 
+            onPress={() => {}}
+          >
+            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
   );
 }
